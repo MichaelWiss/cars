8f90335b67bc9a9635d556c118f8f3ae205658e7
MichaelWiss <michael.wiss@gmail.com>
2023-12-11 17:46:23 -0600

    store

---
+++ src/store/slices/formSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const formSlice = createSlice({
+    name: 'form',
+    initialState: {
+        name: '',
+        const: 0
+    },
+    reducers: {
+        changeName(state, action) {
+
+        },
+        changeCost(state, action) {
+            
+        }
+    }
+});
