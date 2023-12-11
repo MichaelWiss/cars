88f1a4f0083ff26ff111505e249183928fe30969
MichaelWiss <michael.wiss@gmail.com>
2023-12-11 17:50:34 -0600

    state=action

--- src/store/slices/formSlice.js
+++ src/store/slices/formSlice.js
@@ -8,10 +8,13 @@ const formSlice = createSlice({
     },
     reducers: {
         changeName(state, action) {
-
+            state.name = action.payload;
         },
         changeCost(state, action) {
-            
+            state.cost = action.payload;
         }
     }
 });
+
+export const { changeName, changeCost } = formSlice.actions;
+export const formReducer = formSlice.reducer;
