d654949326847be86ec7d77189a167e82d3a2fa0
MichaelWiss <michael.wiss@gmail.com>
2023-12-12 17:09:28 -0600

    carsSlice actions

--- src/store/slices/carsSlice.js
+++ src/store/slices/carsSlice.js
@@ -16,8 +16,21 @@ const carsSlice = createSlice({
             state.cars.push({
                 name: action.payload.name,
                 const: action.payload.cost,
-                id: Math.random()
+                id: nanoid(),
             });
         },
-    }
+        removeCar(state, action) {
+            //Assumption:
+            //action.payload === { }
+            const updated = state.cars.filter((car) => {
+                return car.id !== action.payload
+            });
+            state.cars = updated;
+        },
+    },
 });
+
+export const {
+    changeSearchTerm, addCar, removeCar
+} = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
