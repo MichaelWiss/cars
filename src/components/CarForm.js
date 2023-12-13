5cf8a3c466123d40f9d76fb9c8b76e71047538b2
MichaelWiss <michael.wiss@gmail.com>
2023-12-13 16:40:50 -0600

    carForm

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,5 +1,18 @@
 function CarForm() {
-    return <div>CarForm</div>
+    return <div className="car-form panel">
+        <h4 className="subtitle is-3">Add Car</h4>
+        <form action="">
+            <div className="field-group">
+                <div className="field">
+                    <label htmlFor="" className="label">Name</label>
+                    <input type="text" className="input is-expanded" 
+                    value={name}
+                    onchange={handleNameChange}
+                    />
+                </div>
+            </div>
+        </form>
+    </div>;
 }
 
 export default CarForm;
