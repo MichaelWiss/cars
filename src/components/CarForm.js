192ff573d6134a7a360116d4fd12d2e68396ae48
MichaelWiss <michael.wiss@gmail.com>
2023-12-19 17:59:27 -0600

    handleNameChange

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,4 +1,15 @@
+import { useDispatch } from 'react-redux';
+import { changeName } from '../store';
+
+
 function CarForm() {
+
+
+    const handleNameChange = (event) => {
+        dispatchEvent(changeName(event.target.value));
+    };
+
+
     return <div className="car-form panel">
         <h4 className="subtitle is-3">Add Car</h4>
         <form action="">
@@ -6,8 +17,8 @@ function CarForm() {
                 <div className="field">
                     <label htmlFor="" className="label">Name</label>
                     <input type="text" className="input is-expanded" 
-                    value={name}
-                    onchange={handleNameChange}
+                        value={name}
+                        onchange={handleNameChange}
                     />
                 </div>
             </div>
