ab4c5d2f4d0d8c617e896178b224981097f67d51
MichaelWiss <michael.wiss@gmail.com>
2023-12-22 18:34:46 -0600

    seach space

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,9 +3,11 @@ import { removeCar } from "../store";
 
 function CarList() {
     const dispatch = useDispatch();
-    const cars = useSelector(({ cars: {data, searchTerm }}) => {
-
-        return data.filter((car) => )car.name.toLowerCase().
+    const cars = useSelector(({ cars: {data, searchTerm } }) => {
+        return data.filter((car) => 
+            car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    });
 
     const handleCarDelete = (car) => {
         dispatch(removeCar(car.id));
